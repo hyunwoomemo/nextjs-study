6ebad2072a823ba94ea89b749d97cdb88988a76c
hyunwoomemo <archihw94@gmail.com>
2023-09-03 23:42:37 +0900

    ui: 홈페이지 UI 생성 (하단 플로팅버튼, 데이터 없을 때 표시할 컴포넌트)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image'
 import getProducts, { ProductsParams } from '../actions/getProducts'
+import Container from '../components/Container'
+import EmptyState from '../components/EmptyState'
+import ProductCard from '../components/ProductCard'
+import getCurrentUser from '../actions/getCurrentUser'
+import FloatingButton from '../components/FloatingButton'
 
 interface HomeProps {
   searchParams: ProductsParams
@@ -7,10 +12,36 @@ interface HomeProps {
 
 export default async function Home({ searchParams }: HomeProps) {
   const products = await getProducts(searchParams)
+  const currentUser = await getCurrentUser();
   console.log(products)
   return (
-    <main>
-      누구나 볼 수 있는 페이지입니다.
-    </main>
+    <Container>
+      {/* categories */}
+      {products?.data.length === 0
+        ? 
+        <EmptyState />
+        :
+        <>
+          <div className='grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6'>
+            {
+              products.data.map((product) => {
+                return (
+                  <ProductCard
+                    key={product.id}
+                    currentUser={currentUser}
+                    data={product}
+                  />
+                )
+              })
+            }
+            <ProductCard />
+          </div>
+        </>
+      }
+
+      <FloatingButton
+        href='/products/upload'
+      >+</FloatingButton>
+    </Container>
   )
 }
