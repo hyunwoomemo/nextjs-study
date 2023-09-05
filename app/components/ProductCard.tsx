763520d5f1f91cea9f48351092e8872e6f07bc5c
hyunwoomemo <archihw94@gmail.com>
2023-09-05 23:22:19 +0900

    feat: 상품 등록일 표시 (며칠 전, 몇시간 전) 구현

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useRouter } from 'next/navigation';
 import React from "react";
 import HeartButton from './HeartButton';
+import { fromNow } from '@/helpers/dayjs';
 
 interface ProdcutCardProps {
   data: Product;
@@ -29,11 +30,13 @@ const ProductCard = ({ data, currentUser }: ProdcutCardProps) => {
 
         <div className="text-lg font-semibold ">{data.title}</div>
         <div className="font-light text-neutral-500">{data.category}</div>
-        <div>
+        <div className='flex flex-row items-center justify-between gap-1'>
           <div>
             {data.price} <span className="font-light">원</span>
           </div>
-          <div></div>
+          <div>
+            {fromNow(data.createdAt)}
+          </div>
         </div>
       </div>
     </div>
