4b179a1646bce52c75f86b2c760940b59f5ca79f
hyunwoomemo <archihw94@gmail.com>
2023-09-07 01:05:26 +0900

    feat: 상품 디테일 페이지 구현

---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,31 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import getProductById from '@/app/actions/getProductById'
+import EmptyState from '@/app/components/EmptyState';
+import React from 'react'
+import ProductClient from '../ProductClient';
+
+interface Params {
+  productId?: string
+}
+
+const ProductPage = async ({ params }: { params: Params }) => {
+  
+  const product = await getProductById(params);
+  const currentUser = await getCurrentUser();
+
+  console.log(product)
+
+  if (!product) {
+    return (
+      <EmptyState />
+    )
+  }
+
+  return (
+    <ProductClient product={product} currentUser={currentUser} />
+  )
+}
+
+
+
+export default ProductPage
