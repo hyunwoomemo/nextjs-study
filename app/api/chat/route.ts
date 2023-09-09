59072c4dd545626b0fe7b95be74c9884a96b2fd4
hyunwoomemo <archihw94@gmail.com>
2023-09-10 02:32:51 +0900

    api: 대화 기능 구현을 위한 POST 메소드 정의

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,12 +1,12 @@
-import getCurrentUser from '@/app/actions/getCurrentUser';
-import { NextResponse } from 'next/server';
-import prisma from '@/helpers/prismadb'
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/helpers/prismadb";
 
 export async function GET(request: Request) {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
-    return NextResponse.error()
+    return NextResponse.error();
   }
 
   const users = await prisma.user.findMany({
@@ -19,14 +19,102 @@ export async function GET(request: Request) {
               receiver: true,
             },
             orderBy: {
-              createdAt: 'asc'
-            }
+              createdAt: "asc",
+            },
           },
           users: true,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(users);
+}
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+
+  // 둘이 대화한적 있는지 찾기
+
+  const conversation = await prisma.conversation.findFirst({
+    where: {
+      AND: [
+        {
+          users: {
+            some: {
+              id: body.senderId,
+            },
+          },
+        },
+        {
+          users: {
+            some: {
+              id: body.receiverId,
+            },
+          },
+        },
+      ],
+    },
+  });
+
+  if (conversation) {
+    // 대화한 적 있다면 메시지만 생성
+    try {
+      const message = await prisma.message.create({
+        data: {
+          text: body.text,
+          image: body.image,
+          senderId: body.senderId,
+          receiverId: body.receiverId,
+          conversationId: conversation.id
+        }
+      })
+
+      return NextResponse.json(message)
+
+    } catch (err) {
+      return NextResponse.json(err);
+    }
+  } else {
+    // 처음 대화하는 거라면 message, conversation 둘다 생성
+    const newConversation = await prisma.conversation.create({
+      data: {
+        senderId: body.senderId,
+        receiverId: body.receiverId,
+        users: {
+          connect: [
+            {
+              id: body.senderId
+            },
+            {
+              id: body.receiverId
+            }
+          ]
         }
       }
+    })
+
+    try {
+      const message = await prisma.message.create({
+        data: {
+          text: body.text,
+          image: body.image,
+          senderId: body.senderId,
+          receiverId: body.receiverId,
+          conversationId: newConversation.id
+        }
+      })
+
+      return NextResponse.json(message)
+
+    } catch (err) {
+      return NextResponse.json(err);
     }
-  })
-  
-  return NextResponse.json(users)
+  }
 }
