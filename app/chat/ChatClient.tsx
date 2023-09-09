d358c71c14f1c9f1a0d9f8548a2ed560e6e74264
hyunwoomemo <archihw94@gmail.com>
2023-09-10 01:20:54 +0900

    feat: swr 사용하여 users 데이터 가져오기, Contacts 컴포넌트에 props 전달

--- app/chat/ChatClient.tsx
+++ app/chat/ChatClient.tsx
@@ -1,24 +1,52 @@
 'use client'
+import { TUserWithChat } from '@/types'
 import { User } from '@prisma/client'
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import useSWR from 'swr'
+import Contacts from '../components/chat/Contacts'
 
 interface ChatClientProps {
-  currentUser?: User
+  currentUser?: User | null
 }
 
 const ChatClient = ({ currentUser }: ChatClientProps) => {
-  const [reciver, setRevicer] = useState({
+  const [receiver, setReceiver] = useState({
     receiverId: "",
     receiverName: "",
-    recevierImage: "",
+    receiverImage: "",
   })
 
   const [layout, setLayout] = useState(false);
 
+  const fetcher = (url: string) => axios.get(url).then((res) => res.data)
+  const { data: users, error, isLoading } = useSWR('/api/chat', fetcher, {
+    refreshInterval: 1000
+  })
+
+  console.log(users)
+
+  const currentUserWithMessage = users?.find((user: TUserWithChat) => user.email === currentUser?.email)
+
+  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
+  // useEffect(() => {
+  //   axios.get(`/api/chat`)
+  //     .then((res) => console.log(res));
+  // }, [])
+
   return (
     <main>
       <div className='grid grid-cols-[1fr] md:grid-cols-[300px_1fr]'>
-        <section className={`md:flex ${layout && 'hidden'}`}>Contact Component</section>
+        <section className={`md:flex ${layout && 'hidden'}`}>
+          <Contacts
+            users={users}
+            currentUser={currentUserWithMessage}
+            setLayout={setLayout}
+            setReceiver={setReceiver}
+          />   
+        </section>
         <section className={`md:flex ${!layout && 'hidden'}`}>Chat Component</section>
       </div>
     </main>
